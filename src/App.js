54ceb49d9a2f1d715c916db88084c9ef3ebbae84
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-04-30 14:50:18 +0000

    initial

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MainContainer from './containers/MainContainer/MainContainer'
-function App() {
-  return (
-    <MainContainer></MainContainer>
-  );
-}
-
-export default App;
