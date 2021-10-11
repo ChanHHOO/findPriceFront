6670ac3f97454077aa85587b8589a2f0fec4d097
ChanHHOO <hpyho33@kookmin.ac.kr>
2021-10-12 00:07:25 +0900

    redux store error fix

---
+++ src/pages/Result.js
@@ -0,0 +1,9 @@
+import {React} from "react";
+import ResultContainer from "../containers/ResultContainer/ResultContainer"
+const Result = () => {
+    return(
+        <ResultContainer />
+    )
+}
+
+export default Result;
